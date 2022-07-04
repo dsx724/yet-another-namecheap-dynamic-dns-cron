19807e42e09e5df1127132b008b49375e2fd4702
Da Xue <da.xue@libretech.co>
2022-07-03 23:32:21 -0400

    cleanup the code and readme

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-sudo apt-get install -y dnsutils wget
+# Yet Another Namecheap Dynamic DNS Cron
+## Purpose
+This script updates Namecheap Dynamic DNS records based on the public IP address attached to this host. It is ideal for DMZ scenarios so you can connect remotely to your server from the internet.
 
-*/5 * * * * ~/yet-another-namecheap-dynamic-dns-cron/cron.sh FQDN SECRET
+## Usage
+You need
+
+	sudo apt-get install -y dnsutils wget
+
+crontab to check every 5 minutes:
+
+	*/5 * * * * PUT_ABSOLUTE_PATH_TO_REPO_HERE/yet-another-namecheap-dynamic-dns-cron/cron.sh FQDN SECRET [RESOLVER]
+
+You need to add a resolver IP if your local network domain matches the FQDN domain.
+
+## Design
+1. It looks up the current IP address of the FQDN using the RESOLVER if provided.
+2. It digs OpenDNS servers to find your IP address.
+3. If they do not match, ping the Namecheap DDNS server to update the record using the SECRET.
+
+## Known Issues
+*	This does not account for 3 level domains. Feel free to send a pull request.
+*	This is very simple and may not work for complex (funny) network setups.
 
--- cron.sh
+++ cron.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
-if [ -z "$1" -o -z "$2" ]; then
-	echo "$0 FQDN SECRET"
-	exit 1
-fi
-FQDN="$1"
-SECRET="$2"
-RECORD=${FQDN%.*.*}
-HOST=${FQDN#$RECORD.}
-CURRENT_IP=`nslookup $FQDN | grep '^Address: ' | cut -f 2 -d ' '`
-TARGET_IP=`dig -4 +short myip.opendns.com @resolver1.opendns.com`
-if [ "$CURRENT_IP" != "$TARGET_IP" ]; then
-	echo "Updating Dynamic DNS for $FQDN from $CURRENT_IP to $TARGET_IP."
-	wget -q -o /dev/null -O /dev/null "https://dynamicdns.park-your-domain.com/update?host=$RECORD&domain=$HOST&password=$SECRET"
-fi
+YANDD_lookupFQDN(){ #fqdn resolver
+	if [ -z "$2" ]; then
+		nslookup "$1" | grep '^Address: ' | cut -f 2 -d ' '
+	else
+		nslookup "$1" "$2" | grep '^Address: ' | cut -f 2 -d ' '
+	fi
+}
+YANDD_findMyIP(){
+	dig -4 +short myip.opendns.com @resolver1.opendns.com
+}
+YANDD_update(){ #fqdn target secret
+	local record=${1%.*.*} #TODO: need to account for three component dn
+	local host=${1#$record.}
+	wget -q -o /dev/null -O /dev/null "https://dynamicdns.park-your-domain.com/update?host=$record&domain=$host&password=$3"
+}
+YANDD_main(){
+	if [ -z "$1" -o -z "$2" ]; then
+		echo "$0 FQDN SECRET [RESOLVER]"
+		exit 1
+	fi
+	local ip_current=$(YANDD_lookupFQDN "$1" "$3")
+	local ip_host=$(YANDD_findMyIP)
+	if [ "$ip_current" != "$ip_host" ]; then
+		echo "Updating Namecheap Dynamic DNS for $1 from $ip_current to $ip_host."
+		YANDD_update "$1" "$ip_host" "$2"
+	fi
+}
+YANDD_main "$@"
